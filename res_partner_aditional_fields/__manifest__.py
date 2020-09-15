6fa5aa1da807518e33a51a000cc014930d18385f
adminpacifico <56931073+adminpacifico@users.noreply.github.com>
2020-09-15 13:41:12 -0500

    modificacion res_partener_aditional_fields
    Adicion de campos dias en transito y dia semana para cargue

---
+++ res_partner_aditional_fields/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Campos adicionales cliente',
+    'version': '0.1',
+    'summary': 'Summery',
+    'description': 'Description',
+    'category': 'Category',
+    'author': 'Enrrique Aguiar',
+    'website': 'Website',
+    'depends': ['base', 'contacts'],
+    'data': [
+        'views/res_partner.xml'
+    ],
+}
