97572ad11b765d9d8104106f65a738bae16332a9
adminpacifico <56931073+adminpacifico@users.noreply.github.com>
2020-09-22 14:00:18 -0500

    sale fechas
    Campos para calculo fechas en orden de venta

---
+++ fields_foreign_trade/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Campos adicionales exportacion',
+    'version': '0.1',
+    'summary': 'Summery',
+    'description': 'Description',
+    'category': 'Category',
+    'author': 'Enrrique Aguiar',
+    'website': 'Website',
+    'depends': ['base', 'sale'],
+    'data': [
+        'views/sale.xml',
+        'report/report_carta.xml'
+
+    ],
+}
---
+++ fields_foreign_trade/models/sale.py
@@ -0,0 +1,37 @@
+from datetime import date, timedelta, datetime
+
+from odoo import fields, models, api
+
+class Sales(models.Model):
+    _inherit = 'sale.order'
+    _description = 'calculo de fechas'
+
+    requested_delivery_date = fields.Date('Fecha solicitada de entrega')
+    #days_in_transit = fields.Integer('Days in Transit')
+    possible_upload_date = fields.Char('Posible fecha de cargue', compute='_possible_upload_date')
+    #day_upload_customer = fields.Integer('dato cliente')
+    num_day = fields.Integer('dia')
+    to_days = fields.Integer(string='Dias en transito', related='partner_id.to_days')
+    day_upload_week = fields.Selection(string='Dia de cargue cliente', related='partner_id.day_upload_week')
+    possible_arrival_date_destination = fields.Char('Posible fecha llegada a destino', compute='_possible_arrival_date_destination')
+
+    @api.onchange('requested_delivery_date')
+    def _possible_upload_date(self):
+
+        if self.requested_delivery_date != False:
+            day_upload = self.requested_delivery_date - timedelta(days=self.to_days)
+            self.num_day = day_upload.strftime("%w")
+            if int(self.day_upload_week) > self.num_day:
+               resta = self.num_day - int(self.day_upload_week)
+               self.possible_upload_date = (day_upload - timedelta(days=resta)).strftime("%m-%d-%y-%A")
+            elif int(self.day_upload_week) < self.num_day:
+               resta = int(self.day_upload_week) - self.num_day
+               self.possible_upload_date = (day_upload + timedelta(days=resta)).strftime("%m-%d-%y-%A")
+            else:
+               self.possible_upload_date = day_upload.strftime("%m-%d-%Y-%A")
+        else:
+               self.possible_upload_date = False
+
+    @api.onchange('possible_upload_date')
+    def _possible_arrival_date_destination(self):
+        self.possible_arrival_date_destination = self.possible_upload_date
