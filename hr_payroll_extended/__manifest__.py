5c1864c027e18dd950d5f44274622f09991ee403
adminpacifico <56931073+adminpacifico@users.noreply.github.com>
2020-10-12 17:21:44 -0500

    actualizacion odoo-payroll
    actualizado al 12 de octubre odoo-payroll

--- hr_payroll_extended/__manifest__.py
+++ hr_payroll_extended/__manifest__.py
@@ -27,6 +27,7 @@
         'views/hr_type_payslip_view.xml',
         'views/hr_payslip_run_view.xml',
         'views/hr_rule_parameter_view.xml',
+        'views/hr_leave_view.xml',
         # 'views/hr_payroll_structure_view.xml',
         # 'views/hr_contract_view.xml',
         # 'views/report_contributionregistercust.xml',
--- hr_payroll_extended/models/__init__.py
+++ hr_payroll_extended/models/__init__.py
@@ -3,7 +3,7 @@
 from . import hr_type_payslip
 from . import hr_payroll
 from . import hr_payslip_run
-# from . import hr_leave
+from . import hr_leave
 from . import hr_payslip_input_type
 from . import hr_rule_parameter
 # from . import hr_payroll_structure
