61c01ceec72a47ee33f0723e809f18264a1c1392
adminpacifico <56931073+adminpacifico@users.noreply.github.com>
2020-10-15 19:05:52 -0500

    calculation_delivery_export_etet_v1
    Quitamos el dato %A del strftime y los datos fecha quedaron tipo Date

--- calculation_delivery_export_etet_v1/models/sale.py
+++ calculation_delivery_export_etet_v1/models/sale.py
@@ -7,11 +7,11 @@ class Sales(models.Model):
     _description = 'calculo de fechas'
 
     requested_delivery_date = fields.Date('Fecha solicitada de entrega')
-    possible_upload_date = fields.Char('Posible fecha de cargue')
+    possible_upload_date = fields.Date('Posible fecha de cargue')
     num_day = fields.Integer('dia')
     to_days = fields.Integer(string='Dias en transito', related='partner_id.to_days')
     day_upload_week = fields.Selection(string='Dia de cargue cliente', related='partner_id.day_upload_week')
-    possible_arrival_date_destination = fields.Char('Posible fecha llegada a destino')
+    possible_arrival_date_destination = fields.Date('Posible fecha llegada a destino')
 
     @api.onchange('requested_delivery_date')
     def _possible_upload_date(self):
@@ -25,8 +25,8 @@ class Sales(models.Model):
                 day_upload = day_upload - timedelta(days=1)
                 self.num_day = day_upload.strftime("%w")
 
-            self.possible_upload_date = day_upload.strftime("%m-%d-%Y")
-            self.possible_arrival_date_destination = (day_upload + timedelta(days=self.to_days)).strftime("%m-%d-%Y")
+            self.possible_upload_date = day_upload.strftime("%Y-%m-%d")
+            self.possible_arrival_date_destination = (day_upload + timedelta(days=self.to_days)).strftime("%Y-%m-%d")
         else:
                self.possible_upload_date = False
 
