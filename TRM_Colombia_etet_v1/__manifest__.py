e87665bc8b937f239269c2ffed134fa04ca4473a
adminpacifico <56931073+adminpacifico@users.noreply.github.com>
2020-10-21 16:05:53 -0500

    TRM_colombia_etet_v1
    prueba trm

---
+++ TRM_Colombia_etet_v1/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Consecutivos contenedor',
+    'version': '0.1',
+    'summary': 'Summery',
+    'description': 'Consecutivos contenedor por cliente',
+    'category': 'Category',
+    'author': 'Cesar Quiroga',
+    'website': 'Website',
+    'depends': ['base'],
+    'data': [
+        'views/account_exchange_rate_view.xml',
+    ],
+    'demo': [],
+    'application': True,
+    'installable': True,
+    'auto_install': False
+}
