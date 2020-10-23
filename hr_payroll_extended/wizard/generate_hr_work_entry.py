64fbe80a7a569ed27b0638083bf2d130005c342c
adminpacifico <56931073+adminpacifico@users.noreply.github.com>
2020-10-23 13:07:30 -0500

    cambio en hr_payroll_extended

--- hr_payroll_extended/wizard/generate_hr_work_entry.py
+++ hr_payroll_extended/wizard/generate_hr_work_entry.py
@@ -20,15 +20,19 @@ class GenerateHRWorkEntry(models.TransientModel):
         contract_ids = self.env['hr.contract'].search([('state', '=', 'open')])
         type_ids = self.env['hr.work.entry.type'].search([('code', '=', 'WORK100')], limit=1)
         for contract_id in contract_ids:
+            if not contract_id.employee_id:
+                employee = self.env['hr.employee'].search([('contract_id', '=', contract_id.id)])
+            else:
+                employee = contract_i.employee_id
             work_obj = self.env['hr.work.entry']
             work_id = work_obj.create({
-                'employee_id': contract_id.employee_id.id,
+                'employee_id': employee.id,
                 'work_entry_type_id': type_ids.id,
                 'state': 'draft',
                 'company_id': self.company_id.id,
                 'date_start': self.date_start,
                 'date_stop': self.date_stop,
                 'active': True,
-                'name': contract_id.employee_id.name + ' ' + str(self.date_start) + ' - ' + str(self.date_stop)
+                'name': contract_id.name + ' ' + str(self.date_start) + ' - ' + str(self.date_stop)
             })
         return {'type': 'ir.actions.act_window_close'}
