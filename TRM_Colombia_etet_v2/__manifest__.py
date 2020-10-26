e7f7d769e946907dd57cd692afeae17c87e814bd
adminpacifico <56931073+adminpacifico@users.noreply.github.com>
2020-10-26 11:11:03 -0500

    trm_colombia_etet_v2
    Modulo que actualiza diariamente la trm colombiana vs dollar

---
+++ TRM_Colombia_etet_v2/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'TRM Colombia ETET',
+    'version': '0.1',
+    'summary': 'Summery',
+    'description': 'Updates the exchange rate of Colombia',
+    'category': 'Category',
+    'author': 'Cesar Quiroga',
+    'website': 'Website',
+    'depends': ['base'],
+    'data': [
+        'views/currency_rate_field_cop_views.xml',
+    ],
+    'demo': [],
+    'application': True,
+    'installable': True,
+    'auto_install': False
+}
