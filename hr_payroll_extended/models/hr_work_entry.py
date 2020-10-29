4571353eab4f3d8352499dbffe0c87e96c9c5470
adminpacifico <56931073+adminpacifico@users.noreply.github.com>
2020-10-29 15:50:18 -0500

    hr_payroll_extended
    cambios que estaban en QQ

---
+++ hr_payroll_extended/models/hr_work_entry.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from contextlib import contextmanager
+from dateutil.relativedelta import relativedelta
+
+from odoo import api, fields, models
+
+
+class HrWorkEntry(models.Model):
+    _inherit = 'hr.work.entry'
+
+
+    @api.depends('date_stop', 'date_start')
+    def _compute_duration(self):
+        for work_entry in self:
+            work_entry.duration = work_entry._get_duration(work_entry.date_start, work_entry.date_stop)
+
+
+    def _get_duration(self, date_start, date_stop):
+        if not date_start or not date_stop:
+            return 0
+        dt = date_stop - date_start
+        # return dt.days * 8 + dt.seconds / 3600  # Number of hours
+        return (dt.days+1) * 8  # Number of hours
+
+
+    def _inverse_duration(self):
+        for work_entry in self:
+            if work_entry.date_start and work_entry.duration:
+                print('--')
+                # work_entry.date_stop = work_entry.date_start + relativedelta(hours=work_entry.duration)
---
+++ hr_payroll_extended/wizard/generate_hr_work_entry.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models, _
+
+
+class GenerateHRWorkEntry(models.TransientModel):
+
+    _name = "generate.hr.work.entry"
+
+
+    date_start = fields.Date(
+        string='Fecha de inicio'
+    )
+    date_stop = fields.Date(
+        string='Fecha final'
+    )
+    company_id = fields.Many2one('res.company', string='Company', readonly=True, required=True,
+        default=lambda self: self.env.company)    
+
+    def generate_work_entry(self):
+        contract_ids = self.env['hr.contract'].search([('state', '=', 'open')])
+        type_ids = self.env['hr.work.entry.type'].search([('code', '=', 'WORK100')], limit=1)
+        for contract_id in contract_ids:
+            if not contract_id.employee_id:
+                employee = self.env['hr.employee'].search([('contract_id', '=', contract_id.id)])
+            else:
+                employee = contract_id.employee_id
+            work_obj = self.env['hr.work.entry']
+            work_id = work_obj.create({
+                'employee_id': employee.id,
+                'work_entry_type_id': type_ids.id,
+                'state': 'draft',
+                'company_id': self.company_id.id,
+                'date_start': self.date_start,
+                'date_stop': self.date_stop,
+                'active': True,
+                'name': contract_id.name + ' ' + str(self.date_start) + ' - ' + str(self.date_stop)
+            })
+        return {'type': 'ir.actions.act_window_close'}
