5ae048105cfb6df139778875e1f5c6a5fef4cff2
adminpacifico <56931073+adminpacifico@users.noreply.github.com>
2020-11-23 13:38:20 -0500

    electronic_invoice_validations_etet_v1
    ajustes validacion factura electronica

--- electronic_invoice_validations_etet_v1/models/account_invoice.py
+++ electronic_invoice_validations_etet_v1/models/account_invoice.py
@@ -6,23 +6,25 @@ class AccountMove(models.Model):
     _description = 'Validaciones de plantilla factura electronica'
 
     def action_post(self):
-        if self.partner_id.category_id:
-            get_category = False
-            for category in self.partner_id.category_id:
-                if category.id == 100:
-                    res_company = self.env['res.company'].browse(1)
-                    res_company.update({'l10n_co_edi_template_code': '03'})
-                    get_category = True
-                    break
-                elif category.id == 4:
-                    res_company = self.env['res.company'].browse(1)
-                    res_company.update({'l10n_co_edi_template_code': '01'})
-                    get_category = True
-                    break
-            if not get_category:
-                raise Warning('El cliente debe pertener a la categoría Cliente/Nacional o a la categoría Cliente/Exportación')
-        else:
-            raise Warning('El cliente no tiene asignado una categoría (Cliente/Nacional o Cliente/Exportación)')
+        if self.journal_id.type == 'sale':
+            if self.partner_id.category_id:
+                get_category = False
+                for category in self.partner_id.category_id:
+                    if category.id == 100:
+                        res_company = self.env['res.company'].browse(1)
+                        res_company.update({'l10n_co_edi_template_code': '03'})
+                        get_category = True
+                        break
+                    elif category.id == 4:
+                        res_company = self.env['res.company'].browse(1)
+                        res_company.update({'l10n_co_edi_template_code': '01'})
+                        get_category = True
+                        break
+                if not get_category:
+                    raise Warning(
+                        'El cliente debe pertener a la categoría Cliente/Nacional o a la categoría Cliente/Exportación')
+            else:
+                raise Warning('El cliente no tiene asignado una categoría (Cliente/Nacional o Cliente/Exportación)')
 
         super(AccountMove, self).action_post()
 
