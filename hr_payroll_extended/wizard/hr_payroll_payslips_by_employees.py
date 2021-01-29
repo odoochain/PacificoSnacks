0f749cfe5b028dec9b9ce1e31f564da64144affa
adminpacifico <56931073+adminpacifico@users.noreply.github.com>
2021-01-29 16:41:41 -0500

    actualizacion nomina
    se actualiza a la ultima version a fecha, incluidos cambios realizados por Lorena y Cesar

--- hr_payroll_extended/wizard/hr_payroll_payslips_by_employees.py
+++ hr_payroll_extended/wizard/hr_payroll_payslips_by_employees.py
@@ -1,6 +1,11 @@
 from odoo import _, fields, models
 from odoo.exceptions import UserError
 
+from collections import defaultdict
+from datetime import datetime, date, time
+import pytz
+
+
 
 class HrPayslipEmployees(models.TransientModel):
     _inherit = "hr.payslip.employees"
@@ -65,3 +70,18 @@ class HrPayslipEmployees(models.TransientModel):
             'views': [[False, 'form']],
             'res_id': payslip_run.id,
         }
+
+    def _check_undefined_slots(self, work_entries, payslip_run):
+        """
+        Check if a time slot in the contract's calendar is not covered by a work entry
+        """
+        work_entries_by_contract = defaultdict(lambda: self.env['hr.work.entry'])
+        for work_entry in work_entries:
+            work_entries_by_contract[work_entry.contract_id] |= work_entry
+
+        for contract, work_entries in work_entries_by_contract.items():
+            calendar_start = pytz.utc.localize(datetime.combine(max(contract.date_start, payslip_run.date_start), time.min))
+            calendar_end = pytz.utc.localize(datetime.combine(min(contract.date_end or date.max, payslip_run.date_end), time.max))
+            outside = contract.resource_calendar_id._attendance_intervals(calendar_start, calendar_end) - work_entries._to_intervals()
+            # if outside:
+            #     raise UserError(_("Some part of %s's calendar is not covered by any work entry. Please complete the schedule.") % contract.employee_id.name)
